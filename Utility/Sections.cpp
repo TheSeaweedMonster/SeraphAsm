dafa9d27f70ba316e95ead8099ffb49981a0416e
TheSeaweedMonster <105560604+TheSeaweedMonster@users.noreply.github.com>
2023-10-05 15:39:19 -0700

    Create Sections.cpp

---
+++ Utility/Sections.cpp
@@ -0,0 +1,65 @@
+#include <Windows.h>
+#include "Sections.hpp"
+
+namespace Seraph
+{
+	namespace MemUtil
+	{
+		std::vector<ProcessSection> getSections(const HANDLE hProcess, const uintptr_t pModule)
+		{
+			std::vector<ProcessSection> results = {};
+
+			uint8_t* data = new uint8_t[1000];
+			memset(data, '\0', 1000);
+
+			size_t nbytes;
+
+			ReadProcessMemory(hProcess, reinterpret_cast<void*>(pModule), data, 1000, &nbytes);
+
+			// Size: 0x28 bytes (for x86 and x64 applications)
+			struct SegmentData
+			{
+				char name[8];
+				DWORD size;
+				DWORD offset;
+				DWORD padding[6];
+			};
+
+			uint32_t segmentsStart = 0;
+
+			while (*reinterpret_cast<uint64_t*>(reinterpret_cast<uintptr_t>(data) + segmentsStart) != 0x000000747865742E)
+			{
+				if (segmentsStart >= 0x3F0)
+					break;
+				else
+					segmentsStart += 4;
+			}
+
+			for (auto at = reinterpret_cast<SegmentData*>(reinterpret_cast<uintptr_t>(data) + segmentsStart); at->offset && at->size && segmentsStart < 0x3F0; at++, segmentsStart += sizeof(SegmentData))
+			{
+				Seraph::MemUtil::ProcessSection res;
+				res.name = at->name;
+				res.start = pModule + at->offset;
+				res.end = (pModule + at->offset) + at->size;
+				results.push_back(res);
+			}
+
+			delete[] data;
+
+			return results;
+		}
+
+		ProcessSection getSection(const HANDLE hProcess, const uintptr_t baseModule, const char* const name)
+		{
+			for (const auto& section : getSections(hProcess, baseModule))
+			{
+				if (section.name == name)
+				{
+					return section;
+				}
+			}
+
+			return { "???", 0, 0};
+		}
+	}
+}
